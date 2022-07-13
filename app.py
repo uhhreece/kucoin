439e48271771df1b2157921a94faa3b3f77969b2
uhhreece <reecenorman04@gmail.com>
2022-07-12 16:53:03 -1000

    finished

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ todays_date = datetime.today()
 yesterday = todays_date - timedelta(days=1)
 newdate = yesterday.replace(hour=0, minute=0)
 unixtime = time.mktime(newdate.timetuple())
+start = unixtime - 86400
+
 # print(todays_date)
 # print(yesterday)
 # print(newdate)
-print(unixtime)
+# print(unixtime)
 
-r = requests.get('https://api.kucoin.com/api/v1/market/candles?type=1min&symbol=BTC-USDT&startAt=1657274400&endAt=1657360800')
+r = requests.get(f'https://api.kucoin.com/api/v1/market/candles?type=1min&symbol=BTC-USDT&startAt={int(start)}&endAt={int(unixtime)}')
 # print(r)
 
 string = r.text
@@ -30,7 +32,11 @@ candles = dic['data']
 # print(len(candles))
 
 # print(candles[0])
-# dt = datetime.fromtimestamp(int(candles[0][0]))
-# ts = datetime(2022,7,8,0,0,0).timestamp()
-# print(ts + 86400)
-# print(dic.keys())
+dt = datetime.fromtimestamp(int(candles[0][0]))
+dt2 = datetime.fromtimestamp(int(candles[-1][0]))
+ts = dt.timestamp()
+print(dt)
+print(dt2)
+print(datetime.fromtimestamp(int(ts + 86400)))
+print(dic.keys())
+print(candles.__len__())
